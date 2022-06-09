6b225c33b3877c13b571a6a33324ef81cd548742
Christopher Mogis <102024007+ChrisMogis@users.noreply.github.com>
2022-06-09 11:46:56 +0200

    Update O365-ManageCalendarPermissions.ps1

--- O365-ManageCalendarPermissions.ps1
+++ O365-ManageCalendarPermissions.ps1
@@ -1,9 +1,16 @@
 ################################################################################################
 # This script is used to manage rights on users' calendars on Office 365.                      #
 # Editor : Christopher Mogis                                                                   #
-# Date : 09/06/2022                                                                            #
-# Version 2.0 - Convert select action by drop down menu                                        #
+# Date : 06/03/2022                                                                            #
+# Version 1.1                                                                                  #
 ################################################################################################
+
+Param(
+[Parameter(Mandatory=$true)]
+[ValidateSet("Info", "Add", "Remove")]
+[String[]]
+$Choose
+)
 
 #Set Powershell Execution policy
 Set-ExecutionPolicy RemoteSigned -Force
@@ -57,59 +64,14 @@ If ($result -eq [System.Windows.Forms.DialogResult]::OK)
     $TargetUser
     }
 
-#Select Action
-Add-Type -AssemblyName System.Windows.Forms
-Add-Type -AssemblyName System.Drawing
-
-$form = New-Object System.Windows.Forms.Form
-$form.Text = 'Select Action'
-$form.Size = New-Object System.Drawing.Size(300,200)
-$form.StartPosition = 'CenterScreen'
-$form.Icon = 'C:\Temp\favicon-image.ico'
-
-$okButton = New-Object System.Windows.Forms.Button
-$okButton.Location = New-Object System.Drawing.Point(105,120)
-$okButton.Size = New-Object System.Drawing.Size(75,23)
-$okButton.Text = 'OK'
-$okButton.DialogResult = [System.Windows.Forms.DialogResult]::OK
-$form.AcceptButton = $okButton
-$form.Controls.Add($okButton)
-
-$label = New-Object System.Windows.Forms.Label
-$label.Location = New-Object System.Drawing.Point(10,20)
-$label.Size = New-Object System.Drawing.Size(280,20)
-$label.Text = 'Please select the permission :'
-$form.Controls.Add($label)
-$listBox = New-Object System.Windows.Forms.ListBox
-$listBox.Location = New-Object System.Drawing.Point(10,40)
-$listBox.Size = New-Object System.Drawing.Size(260,20)
-$listBox.Height = 80
-
-[void] $listBox.Items.Add('Info')
-[void] $listBox.Items.Add('Add')
-[void] $listBox.Items.Add('Remove')
-
-$form.Controls.Add($listBox)
-
-$form.Topmost = $true
-
-$Select = $form.ShowDialog()
-
-if ($Select -eq [System.Windows.Forms.DialogResult]::OK)
-{
-    $Action = $listBox.SelectedItem
-    $Action
-}
-
-#Targeted User Information
-If ($Action -eq "Info")
+If ($Choose -eq "Info")
     {
 #List all calendar with all permissions
 #Get-Mailbox -Identity $TargetUser | Get-MailboxFolderStatistics -FolderScope Calendar | ft Identity,Name
 Get-MailboxFolderPermission -Identity "$($TargetUser):\Calendrier" | ft Identity,FolderName,User,AccessRights
     }
 
-If ($Action -eq "Add")
+If ($Choose -eq "Add")
     {
 #Delegated User Information
 Add-Type -AssemblyName System.Windows.Forms
@@ -207,7 +169,7 @@ Add-MailboxFolderPermission -Identity "$($TargetUser):\Calendrier" -User $Delega
     }
 
 #Delete Calendar Permission
-If ($Action -eq "Remove")
+If ($Choose -eq "Remove")
 {
 #Delegated User Information
 Add-Type -AssemblyName System.Windows.Forms
